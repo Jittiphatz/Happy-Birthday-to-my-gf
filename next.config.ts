eca8e4c9ff1fd3969efcd7b895f7b3b52fcd3d31
Jittiphatz <bmstudio2551the@gmail.com>
2025-03-30 23:19:29 +0700

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.(woff|woff2)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'fonts/',
-          },
-        },
-      });
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
